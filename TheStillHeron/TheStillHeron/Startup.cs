ada566dc3e419c44a0b701ab1681f02f2612a8db
Jamie Butler <jamie.b@iinet.net.au>
2015-07-16 18:59:26 +0800

    Initial commit. Built from VS MVC template

---
+++ TheStillHeron/TheStillHeron/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheStillHeron.Startup))]
+namespace TheStillHeron
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
