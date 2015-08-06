bfef06c70a3edb60e579568ee727f91db3d21473
Jamie Butler <jamie.b@iinet.net.au>
2015-08-06 16:22:36 +0800

    Adding new generated files

---
+++ TheStillHeron/TheStillHeron/Scripts/Site.js
@@ -0,0 +1,18 @@
+﻿var banner_height;
+
+function handleScroll(event) {
+    var scrolled = $(window).scrollTop();
+    if (banner_height - scrolled < 0) {
+        $(".navbar-fixed-top").css("top", 0);
+    }
+    else {
+        $(".navbar-fixed-top").css("top", banner_height - scrolled);
+    }
+}
+
+$(document).ready(function () {
+    banner_height = $(".navbar-fixed-top").css("top");
+    banner_height = banner_height.replace("px", "");
+
+    $(window).scroll(handleScroll);
+});
